2774f5dfbaf5fb28851476b8ed184e2c0f0e2467
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2025-02-19 15:34:48 +1100

    pre chapter 10 partial rerendering. experimental, might even just skip for now if it doesn't effect the rest of the project...

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,8 +16,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    //console.log('Fetching revenue data...');
+    //await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
@@ -33,8 +33,8 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
 
-    console.log('Fetching invoice data...');
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    //console.log('Fetching invoice data...');
+    //await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -65,6 +65,9 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
+
+    //console.log('Fetching card data...');
+    //await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const data = await Promise.all([
       invoiceCountPromise,
