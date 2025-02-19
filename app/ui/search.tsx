1e83173623556992b683839bb4a3fe063852032d
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2025-02-19 16:32:29 +1100

    fix placeholder search param

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,7 +3,7 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
-export default function Search() {
+export default function Search({ placeholder }: { placeholder: string }) {
   
   const searchParams = useSearchParams();
   const pathname = usePathname();
@@ -26,10 +26,11 @@ export default function Search() {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder="{placeholder}"
+        placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
